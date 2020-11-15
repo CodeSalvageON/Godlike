7d915870028dd506356e64ab7a58f1e842a19a64
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2020-11-15 14:27:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function createGUI () {
   document.getElementById("background-image").src = "https://media.discordapp.net/attachments/757231447172972606/777658230380822538/81uilGLQYBL.png?width=671&height=671";
   document.getElementsByTagName("head")[0].appendChild(favicon);
   document.querySelector("body").style.backgroundColor = "black";
+  document.querySelector("body").style.color = "red";
 }
 
 createGUI();

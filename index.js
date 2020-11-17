47e7241ceb17343046526d11e888ea77da215443
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2020-11-16 23:02:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ favicon.rel = "shortcut icon";
 favicon.href = "https://media.discordapp.net/attachments/772064957793435678/777660399821652008/bomb_PNG16.png";
 
 function createGUI () {
-  document.getElementById("background-image").src = "https://media.discordapp.net/attachments/757231447172972606/777658230380822538/81uilGLQYBL.png?width=671&height=671";
   document.getElementsByTagName("head")[0].appendChild(favicon);
   document.querySelector("body").style.backgroundColor = "black";
   document.querySelector("body").style.color = "red";

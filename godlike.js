6d9496948f9ea3006ccd3ba224f8d41894145525
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2020-11-21 05:43:32 -0800

    Update godlike.js

--- godlike.js
+++ godlike.js
@@ -2,6 +2,7 @@
 
 const command_list = `
 help - Lists all commands.
+change_icon - Allows the user to change the page icon.
 `;
 
 function createShell () {
@@ -10,5 +11,16 @@ function createShell () {
   
   if (shell_prompt.toLowerCase() === "help") {
     alert(command_list);
+  }
+  
+  else if (shell_prompt.toLowerCase() === "change_icon") {
+    const icon_prompt = prompt("Icon Image Link: ");
+    const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = icon_prompt;
+    
+    document.getElementsByTagName('head')[0].appendChild(link);
   }
 }
